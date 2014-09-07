a1d2392a595fab8dbb6e6fe17204f989abac81e6
Stijn Seghers <seghers_stijn@hotmail.com>
2014-09-07 17:11:09 +0200

    Added dev_server and deploy script

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+appcfg.py -e "stijn.seghers@gmail.com" update ~/Dropbox/Liefje/TLM/the-little-monks
---
+++ dev_server.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+dev_appserver.py --log_level debug ~/Dropbox/Liefje/TLM/the-little-monks/
