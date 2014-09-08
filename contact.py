580370a41d24230130b12aaf76b5bc9dea17db40
Stijn Seghers <seghers_stijn@hotmail.com>
2014-09-08 13:12:46 +0200

    Minor changes, fixes #18

--- contact.py
+++ contact.py
@@ -24,14 +24,12 @@ class ContactPage(webapp2.RequestHandler):
         birth_year = int(self.request.get('birth[year]'))
         subject = self.request.get('subject')
         comment = self.request.get('comment')
-        if not all((name, email, subject, comment)):
+        if not all((email, subject, comment)):
             return error(self, 'You didn\'t supply everything.')
         if not mail.is_email_valid(email):
             return error(self, 'You entered an incorrect email address.')
         birth_date = '%02d/%02d/%04d' % (birth_day, birth_month, birth_year)
         body = BODY % (name, email, birth_date, comment)
-        import logging
-        logging.debug(body)
         mail.send_mail(sender=SENDER, to=TO, subject=SUBJECT % subject + body,
                        reply_to=email, body=body)
         render_page(self, 'contact_thanks.html')
