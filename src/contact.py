7a94946ddf0902e4fdf8cc31943ea7b8a80fd5b6
Stijn Seghers <seghers_stijn@hotmail.com>
2016-01-08 07:31:43 +0100

    Be more lenient with birthdates

--- src/contact.py
+++ src/contact.py
@@ -19,16 +19,16 @@ class ContactPage(webapp2.RequestHandler):
     def post(self):
         name = self.request.get('name')
         email = self.request.get('email')
-        birth_day = int(self.request.get('birth[day]'))
-        birth_month = int(self.request.get('birth[month]'))
-        birth_year = int(self.request.get('birth[year]'))
+        birth_day = self.request.get('birth[day]')
+        birth_month = self.request.get('birth[month]')
+        birth_year = self.request.get('birth[year]')
         subject = self.request.get('subject')
         comment = self.request.get('comment')
         if not all((email, subject, comment)):
             return error(self, 'You didn\'t supply everything.')
         if not mail.is_email_valid(email):
             return error(self, 'You entered an incorrect email address.')
-        birth_date = '%02d/%02d/%04d' % (birth_day, birth_month, birth_year)
+        birth_date = '%s/%s/%s' % (birth_day, birth_month, birth_year)
         body = BODY % (name, email, birth_date, comment)
         mail.send_mail(sender=SENDER, to=TO, subject=SUBJECT % subject + body,
                        reply_to=email, body=body)
